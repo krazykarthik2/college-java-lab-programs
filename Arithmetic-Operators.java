5754bbe6a849b8e996f54ed30438b1f01566dc1f
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-26 11:15:14 +0530

    Create Arithmetic-Operators.java

---
+++ Arithmetic-Operators.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+class Arith{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        System.out.println("x+y="+(x+y));
+        System.out.println("x-y="+(x-y));
+        System.out.println("x*y="+(x*y));
+        System.out.println("x/y="+(x/y));
+        System.out.println("x%y="+(x%y));
+    }
+    
+}
