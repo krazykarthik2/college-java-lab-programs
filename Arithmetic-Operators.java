67f32321b6c4db7aa1f2f6a819fca90e8ea1db7f
Karthik Goparaju <72034219+krazykarthik2@users.noreply.github.com>
2024-07-26 11:27:21 +0530

    Update Arithmetic-Operators.java

--- Arithmetic-Operators.java
+++ Arithmetic-Operators.java
@@ -2,14 +2,15 @@ import java.util.*;
 import java.lang.*;
 class Arith{
     public static void main(String[] args){
+        System.out.println("Enter two numbers: ");
         Scanner scan = new Scanner(System.in);
         int x = scan.nextInt();
         int y = scan.nextInt();
-        System.out.println("x+y="+(x+y));
-        System.out.println("x-y="+(x-y));
-        System.out.println("x*y="+(x*y));
-        System.out.println("x/y="+(x/y));
-        System.out.println("x%y="+(x%y));
+        System.out.println("the value of x+y is :"+(x+y));
+        System.out.println("the value of x-y is :"+(x-y));
+        System.out.println("the value of x*y is :"+(x*y));
+        System.out.println("the value of x/y is :"+(x/y));
+        System.out.println("the value of x%y is :"+(x%y));
     }
     
 }
